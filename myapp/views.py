003a44f70946913e686f22bebde0e9371016415b
Mahammmadhusain Kadiwala <kadiwala.530@gmail.com>
2023-02-21 19:24:00 +0530

    render data on html

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
 
 # Create your views here.
 
@@ -7,14 +8,17 @@ from pytube import YouTube
 def HomeView(request):
 
     video_url = YouTube('https://www.youtube.com/watch?v=XhEw22gxiEI')
-
+    
+    print(video_url)
+    
     video_title = video_url.title
     video_thumbnail = video_url.thumbnail_url
     all_available_qualities = video_url.streams
 
     streams_list = []
-
+    # .filter(progressive=True)
     for i in all_available_qualities:
+        print(vars(i))
         video_details = vars(i)
         streams_list.append(
             {
@@ -23,13 +27,16 @@ def HomeView(request):
                 'download_url': video_details['url'],
                 'video_quality': video_details['subtype'],
                 'filesize_mb': video_details['_filesize_mb'],
+                'file_type': video_details['type'],
                 'filesize_gb': video_details['_filesize_gb'],
                 'resolution': video_details['resolution'],
             }
         )
-    print(streams_list)
+
+    # print(streams_list)
     context = {
         'video_title': video_title,
         'video_thumbnail': video_thumbnail,
+        'streams_list':streams_list,
     }
     return render(request, 'index.html', context)
