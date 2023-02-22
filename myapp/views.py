7255a19cfcd0a2d4a042b923e6a43453fc9fa650
Mahammmadhusain Kadiwala <kadiwala.530@gmail.com>
2023-02-22 10:53:11 +0530

    single video done

--- myapp/views.py
+++ myapp/views.py
@@ -7,36 +7,50 @@ from pytube.exceptions import VideoUnavailable
 
 def HomeView(request):
 
-    video_url = YouTube('https://www.youtube.com/watch?v=XhEw22gxiEI')
-    
-    print(video_url)
-    
-    video_title = video_url.title
-    video_thumbnail = video_url.thumbnail_url
-    all_available_qualities = video_url.streams
+    try:
+        video_url = YouTube('https://www.youtube.com/playlist?list=PLbGui_ZYuhigchy8DTw4pX4duTTpvqlh6')
 
-    streams_list = []
-    # .filter(progressive=True)
-    for i in all_available_qualities:
-        print(vars(i))
-        video_details = vars(i)
-        streams_list.append(
-            {
-                'file_type': video_details['type'],
-                'mime_type': video_details['mime_type'],
-                'download_url': video_details['url'],
-                'video_quality': video_details['subtype'],
-                'filesize_mb': video_details['_filesize_mb'],
-                'file_type': video_details['type'],
-                'filesize_gb': video_details['_filesize_gb'],
-                'resolution': video_details['resolution'],
-            }
-        )
+        author = video_url.author
+        publish_date = video_url.publish_date
+        channel_url = video_url.channel_url
+        description = video_url.description
+        # print(video_url.keywords)
+        
+        video_title = video_url.title
+        video_thumbnail = video_url.thumbnail_url
+        all_available_qualities = video_url.streams
 
-    # print(streams_list)
-    context = {
-        'video_title': video_title,
-        'video_thumbnail': video_thumbnail,
-        'streams_list':streams_list,
-    }
-    return render(request, 'index.html', context)
+        streams_list = []
+        # .filter(progressive=True)
+        for i in all_available_qualities:
+            # print(vars(i))
+            video_details = vars(i)
+            streams_list.append(
+                {
+                    'file_type': video_details['type'],
+                    'mime_type': video_details['mime_type'],
+                    'download_url': video_details['url'],
+                    'video_quality': video_details['subtype'],
+                    'filesize_mb': video_details['_filesize_mb'],
+                    'file_type': video_details['type'],
+                    'filesize_gb': video_details['_filesize_gb'],
+                    'resolution': video_details['resolution'],
+                    'abr': video_details['abr'],
+                }
+            )
+
+        # print(streams_list)
+        context = {
+            'video_title': video_title,
+            'video_thumbnail': video_thumbnail,
+            'streams_list':streams_list,
+            # -------------------------
+            'author':author,
+            'publish_date':publish_date,
+            'channel_url':channel_url,
+            'description':description,
+        }
+        return render(request, 'index.html', context)
+    except:
+        print("***********")
+        return render(request, 'index.html')
